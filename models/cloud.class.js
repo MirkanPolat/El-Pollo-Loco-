debd09bde47a200ad598726ccf728f3b96a31feb
MirkanPolat <Mirkanpolat@outlook.de>
2025-05-06 14:55:33 +0200

    fixing clouds problem like reprint new clouds as the actual cloud is left the game

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,10 +7,17 @@ class Cloud extends MovableObject {
     constructor() {
         super().loadImage('./img/5_background/layers/4_clouds/1.png');
         this.x = Math.random() * 1500;
+        this.speed = 0.2 + Math.random() * 0.5;
         this.animate();
     }
-    animate() {
+animate() {
+    setInterval(() => {
         this.moveLeft();
-    }
 
+        // Wenn Wolke komplett aus dem Bild verschwunden ist:
+        if (this.x < -this.width) {
+            this.x = 1500 + Math.random() * 500; // neu erscheinen rechts
+        }
+    }, 40); // 25 FPS (kannst auch 1000 / 60 nehmen)
+}
 }
