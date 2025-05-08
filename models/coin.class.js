309303e67fb4580080df625d0aa9f999616cceda
MirkanPolat <Mirkanpolat@outlook.de>
2025-05-08 13:02:13 +0200

    fixing the offset for coins

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,6 +1,14 @@
 class Coin extends MovableObject {
   height = 120;
   width = 120;
+
+  offset = {
+    top: 40,
+    left: 40,
+    right: 40,
+    bottom: 40
+  };
+
   IMAGES_COIN = [
     "./img/8_coin/coin_1.png",
     "./img/8_coin/coin_2.png"
